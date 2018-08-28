f7bd7464efa22e39783c7fae4f2cd85019e36f31
Maciej <31264447+Kratak@users.noreply.github.com>
2018-08-28 14:14:44 +0100

    random picker working, simple one file version

--- src/App.js
+++ src/App.js
@@ -2,12 +2,26 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state={
+      cardNumber: 1234,
+      cardLimit: 100
+    };
+  }
   render() {
     return (
       <div className="App">
-      sss
+      <button onClick={this.randomCard.bind(this)}>Losuj Fish/ke</button>
+      <br/>
+      <output>{this.state.cardNumber}</output>
       </div>
     );
+  }
+  randomCard(){
+this.setState(
+  {cardNumber: Math.floor((Math.random() * this.state.cardLimit)+ 0) }
+)
   }
 }
 
