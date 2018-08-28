5ddf1b4ecb74ac76684384bb26662a3a5bd84da2
Maciej <31264447+Kratak@users.noreply.github.com>
2018-08-28 15:38:53 +0100

    ranndom picer, from this.state[this.state.cardnumber]

--- src/App.js
+++ src/App.js
@@ -1,28 +1,43 @@
 import React, { Component } from 'react';
 import './App.css';
 
+
+
 class App extends Component {
   constructor(){
     super();
     this.state={
-      cardNumber: 1234,
-      cardLimit: 100
+      cardNumber:0,
+      cardLimit: 4,
+      1: '1.Pierwszy tekst',
+      2: '2.drugi tekst',
+      3: '3.trzeci tekst',
+      4: '4.czawrty tekst',
     };
   }
   render() {
     return (
       <div className="App">
+      <br/>
       <button onClick={this.randomCard.bind(this)}>Losuj Fish/ke</button>
       <br/>
-      <output>{this.state.cardNumber}</output>
+      <br/>
+      <output>Text number: <br/>&nbsp; {this.state[this.state.cardNumber]}</output>
+      <p>How its works? "onlick" generte number from 1 - {this.state.cardLimit}, <br/>
+      passing this number to"this.state[this.state.cardNumber]",<br/>
+      and render message on scren. </p>
+      <br/>
       </div>
+      
     );
+
   }
   randomCard(){
 this.setState(
-  {cardNumber: Math.floor((Math.random() * this.state.cardLimit)+ 0) }
+  {cardNumber: Math.floor((Math.random() * this.state.cardLimit)+ 1) }
 )
   }
+  
 }
 
 export default App;
---
+++ src/cards/1/card.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Card extends Comment{
+    render(){
+    return(
+        <div>some text</div>
+    )
+}
+}
+
+
+export default Card;
